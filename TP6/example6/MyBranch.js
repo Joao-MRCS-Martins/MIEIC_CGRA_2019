426fe003351a88a068a8b685779cfc0cacef704f
João Martins <up201707311@fe.up.pt>
2019-05-02 16:47:46 +0100

    changed shininess

--- TP6/example6/MyBranch.js
+++ TP6/example6/MyBranch.js
@@ -5,7 +5,7 @@ class MyBranch {
         //trunk material
         this.woodAppearance = new CGFappearance(scene);
         this.woodAppearance.setDiffuse(168/255,120/255,0,1);
-        this.woodAppearance.setShininess(80);
+        this.woodAppearance.setShininess(100);
     }
 
     display() {
--- TP6/example6/MyLeaf.js
+++ TP6/example6/MyLeaf.js
@@ -6,7 +6,7 @@ class MyLeaf {
         this.green = new CGFappearance(scene);
         this.green.setDiffuse(0,100/255,0,1);
         this.green.setSpecular(0,50/255,0,1);
-        this.green.setShininess(15);
+        this.green.setShininess(100);
     }
 
     display() {
